5dec874426530af309a96f8857014271e4fb2830
aSwanting <gab.damico@gmail.com>
2023-10-18 14:13:19 +0200

    add: array of team member objects (milestone 1)

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const teamMember = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        portrait: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        portrait: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        portrait: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        portrait: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        portrait: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        portrait: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+
+console.log(teamMember)
+// debugger
